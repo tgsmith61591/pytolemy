5003804e240fd615d1cdf599c829fc7ac021b1f5
Gigi Weingarden <gigi.weingarden.ee9w@statefarm.com>
2018-09-18 12:39:36 -0500

    Add to __init__.py

--- pytolemy/utils/convert_image_format.py
+++ pytolemy/utils/convert_image_format.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import absolute_import
-
+#from osgeo import gdal
+#import gdaltest
 from PIL import Image
 import numpy as np
+import os
 
 __all__ = [
     'jp2_to_tiff',
@@ -11,11 +13,18 @@ __all__ = [
 ]
 
 def jp2_to_tiff(jp2_image_path):
-    im = Image.open(jp2_image_path)
-    imarray = np.array(im) # converts to array
-    #im.show()
+    file_name = os.path.splitext(jp2_image_path)[0]
+
+
+ #   input_image = gdal.Open(jp2_image_path)
+ #   output_image = gdal.Translate('output.tif', input_image)
+
+ #   if output_image is None:
+ #       gdaltest.post_reason('got error/warning')
+ #       return 'fail'
+
 
 def tiff_to_jp2(tiff_image_path):
     im = Image.open(tiff_image_path)
     out = im.convert("RGB")
-    #out.save(outfile, "JPEG", quality=90)
+
