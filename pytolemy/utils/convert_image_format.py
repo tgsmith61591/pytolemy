865cc69585b8136a06822e3f5b3cbcc0a6265bb7
Gigi Weingarden <gigi.weingarden.ee9w@statefarm.com>
2018-09-20 14:05:13 -0500

    adding check path statements

--- pytolemy/utils/convert_image_format.py
+++ pytolemy/utils/convert_image_format.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import absolute_import
-#from osgeo import gdal
-#import gdaltest
 from PIL import Image
-
 import numpy as np
 import os
 
@@ -13,18 +10,17 @@ __all__ = [
     'tiff_to_jp2'
 ]
 
+
 def jp2_to_tiff(jp2_image_path):
-    file_name = os.path.splitext(jp2_image_path)[0]
-
- #   input_image = gdal.Open(jp2_image_path)
- #   output_image = gdal.Translate('output.tif', input_image)
-
- #   if output_image is None:
- #       gdaltest.post_reason('got error/warning')
- #       return 'fail'
+    check_if_file_exists(jp2_image_path)
 
 
 def tiff_to_jp2(tiff_image_path):
-    im = Image.open(tiff_image_path)
-    out = im.convert("RGB")
+    check_if_file_exists(tiff_image_path)
 
+
+def check_if_file_exists(path):
+    try:
+        os.path.isfile(path)
+    except FileNotFoundError:
+        raise IOError("File not found!")
--- pytolemy/utils/tests/test_image_format_converter.py
+++ pytolemy/utils/tests/test_image_format_converter.py
@@ -3,12 +3,15 @@
 from __future__ import absolute_import
 from pytolemy.utils.convert_image_format import tiff_to_jp2, jp2_to_tiff
 import pytest
+import os
 
 
-def test_jp2_to_tiff():  # Check if tiff type image
-    print('test')
-    #jp2_to_tiff('19TCG240845/19TCG240845.jp2')
+def test_jp2_to_tiff():
+    path = os.getcwd() + ' "*.jp2"'
+    jp2_to_tiff(path)
 
-def test_tiff_to_jp2():# Check if tiff type image
-    print('test')
-   # tiff_to_jp2('19TCG240845/19TCG240845.tif')
+
+def test_tiff_to_jp2():
+    path = os.getcwd() + ' "*.tif"'
+    tiff_to_jp2(path)
+
